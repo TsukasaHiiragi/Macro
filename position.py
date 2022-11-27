9c6253052a89ff7750881713e164f616be832e8e
AtsushiMiyashita317 <miyashita.atsushi@g.sp.m.is.nagoya-u.ac.jp>
2022-11-27 16:30:53 +0900

    new files

---
+++ position.py
@@ -0,0 +1,26 @@
+from time import sleep
+import pyautogui
+from pynput import mouse
+
+class Monitor:
+    def __init__(self):
+        self.x,self.y = None,None
+
+    def start(self):
+        with mouse.Listener(on_click=self.click) as mouse_listener:
+            mouse_listener.join()
+
+    def click(self, x, y, button, pressed):
+        if button.name == 'left':
+            self.x,self.y = x,y
+        return False
+
+dx = [0,635,635,1270,1270]
+dy = [520,0,520,0,520]
+while 1:
+    monitor = Monitor()
+    monitor.start()
+    if monitor.x is not None:
+        print(monitor.x,monitor.y)
+    else:
+        break
---
+++ tmp2.py
@@ -0,0 +1,7 @@
+import subprocess
+
+subprocess.run(
+    ["start",
+     "C:/PROGRA~1/Google/Chrome/Application/chrome.exe",
+     "--user-data-dir=C:/tfall01"]
+    ,shell=True)
